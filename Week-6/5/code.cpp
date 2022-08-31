eaa8760b01596d52f377ab7673e9c642d2ea475b
Pradip Bedre <bedrepradip0@gmail.com>
2022-08-31 23:17:49 +0530

    question 5 compeated buy sell stock

---
+++ Week-6/5/code.cpp
@@ -0,0 +1,5 @@
+/* Question: Pair With Given Difference */
+/* Approach1: Sort and Binary search array and check element and element+1 if  
+   Approach2: we can use two loop the outer loop picks the first element (smaller element) and the inner loop looks for the element picked by outer loop plus n. Time complexity of this method is O(n2).
+   
+*/
