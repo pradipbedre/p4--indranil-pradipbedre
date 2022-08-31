bcf931a69ffb3a528193fd29dca288b81389a469
Pradip Bedre <bedrepradip0@gmail.com>
2022-08-31 23:22:11 +0530

    question 5 compleated

--- Week-6/5/code.cpp
+++ Week-6/5/code.cpp
@@ -1,5 +1,39 @@
 /* Question: Pair With Given Difference */
-/* Approach1: Sort and Binary search array and check element and element+1 if  
+/* Approach1: Sort and Binary search array and check
+ element and element+1 if  time: o(n.logn) space o(1)
    Approach2: we can use two loop the outer loop picks the first element (smaller element) and the inner loop looks for the element picked by outer loop plus n. Time complexity of this method is O(n2).
-   
+
 */
+#include <bits/stdc++.h>
+using namespace std;
+
+bool findPair(int arr[], int size, int n)
+{
+   int i = 0;
+   int j = 1;
+
+   while (i < size && j < size)
+   {
+      if (i != j && (arr[j] - arr[i] == n || arr[i] - arr[j] == n))
+      {
+         cout << "Pair Found: (" << arr[i] << ", " << arr[j] << ")";
+         return true;
+      }
+      else if (arr[j] - arr[i] < n)
+         j++;
+      else
+         i++;
+   }
+
+   cout << "No such pair";
+   return false;
+}
+
+int main()
+{
+   int arr[] = {1, 8, 30, 40, 100};
+   int size = sizeof(arr) / sizeof(arr[0]);
+   int n = 60;
+   findPair(arr, size, n);
+   return 0;
+}
