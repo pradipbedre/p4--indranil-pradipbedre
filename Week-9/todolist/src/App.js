f6705a3a08d5998b600251116c5dea4ec3c8815f
Pradip Bedre <bedrepradip0@gmail.com>
2022-09-24 19:40:17 +0530

    starting our app

--- Week-9/todolist/src/App.js
+++ Week-9/todolist/src/App.js
@@ -1,8 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { faCircleCheck, FaPen, faTrashCan } from '../node_modules/react-icons/fa';
 import './App.css';
-import TodoForm from './components/TodoForm';
-export default function App() {
+function App() {
+
+  // task todo list state / main state to store tasks 
+  const[toDo,setToDo]=useState([
+    {"id":1,"title": "Task 1","status": false},
+    {"id":2,"title": "Task 2","status": false}
+  ]);
+
+  // temp state for when we will update tasks 
+  const [newTask,setNewTask]=useState('');
+  const [updateData,setupdateData]=useState('');
+
+// add task
+////////////////////////////
+  const addTask=()=>{
+
+  }
+
+  // delete task
+////////////////////////////
+  const deleteTask=()=>{
+
+  }
+// mark task as done compleated
+////////////////////////////
+  const markDone=()=>{
+
+  }
+
+  // cancle updated ask
+////////////////////////////
+  const cancleUpdate=()=>{
+
+  }
+
+  // change task
+////////////////////////////
+  const changeTask=(e)=>{
+
+  }
+
+  // update task 
+////////////////////////////
+  const updateTask=()=>{
+
+  }
+
   return (
-    <><TodoForm/></>
+    <div className='container App'>
+      <br /><br />
+      <h2>To Do List App Using React.JS</h2>
+      <br /><br />
+    </div>
   )
 }
+
+export default App;
